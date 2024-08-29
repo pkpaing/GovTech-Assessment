9b866b9d3fd67c5806a373eaed5bcb883351d4d2
pkpaing <pkpaing@gmail.com>
2024-08-29 23:11:31 +0800

    Modified configs for filepaths

--- config.py
+++ config.py
@@ -5,9 +5,14 @@ class Config:
     """Configurations for data ingestion"""
     restaurant_filepath: str
     country_code_filepath: str
+    restaurants_save_filepath: str
+    restaurant_events_save_filepath: str
+    
 
 
 configs = Config(
-    restaurant_filepath="data/restaurant_data.json",
-    country_code_filepath="data/Country-Code.xlsx"
+    restaurant_filepath="source_data/restaurant_data.json",
+    country_code_filepath="source_data/Country-Code.xlsx",
+    restaurants_save_filepath="output_data/restaurants.csv",
+    restaurant_events_save_filepath="output_data/restaurant_events.csv"
 )
